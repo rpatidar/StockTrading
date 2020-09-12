bc676162d58dcfacc33533ef15d4626283557c37
Rajesh Patidar <patidarchat@gmail.com>
2020-09-12 15:10:44 +0530

    Adding the session login/restore
    Adding the session login/restore to avoid repeated logins

--- broker/zerodha/zeroda_base.py
+++ broker/zerodha/zeroda_base.py
@@ -2,13 +2,14 @@
 from kiteconnect import KiteConnect
 from broker.trading_base import TradingService
 import os, pickle, webbrowser
-
+import datetime
 
 tmp_dir = "c:/tmp/" if os.name == "nt" else "/tmp/"
 mock_file = tmp_dir +"/mock"
 
 class ZerodhaServiceBase(TradingService):
     def __init__(self, credential, configuration):
+        self.session_file = tmp_dir + "/session_file"
         super(ZerodhaServiceBase, self).__init__(credential, configuration)
         self.api_key = credential['api_key']
         self.api_secret = credential['api_secret']
@@ -17,16 +18,36 @@ class ZerodhaServiceBase(TradingService):
         self.tmp_dir  = tmp_dir
         self.instrument_file = tmp_dir+"/instruments"
 
-
     def _login(self):
         if self.mock == 0:
             kite = KiteConnect(self.api_key, self.api_secret)
-            webbrowser.open_new_tab(KiteConnect(self.api_key, self.api_secret).login_url())
-            url = input("Enter Your token URL here")
-            data = kite.generate_session(url.split("request_token=")[1].split("&action")[0], api_secret=self.api_secret)
-            kite.set_access_token(data["access_token"])
-            self.access_token = data["access_token"]
+            token = self._load_token();
+            if not token:
+                webbrowser.open_new_tab(KiteConnect(self.api_key, self.api_secret).login_url())
+                url = input("Enter Your token URL here")
+                tmp_dir + "/session_file"
+                data = kite.generate_session(url.split("request_token=")[1].split("&action")[0], api_secret=self.api_secret)
+                token = data["access_token"]
+                self._save_session(token)
+            kite.set_access_token(token)
+            self.access_token = token
             self.kite = kite
+
+    def _load_token(self):
+
+        if not os.path.exists(self.session_file):
+            return None
+        session_data = pickle.load(open(self.session_file, "rb"))
+
+        if ( session_data['lastSessionDate'] + datetime.timedelta(days=-1)  < datetime.datetime.now() ):
+            return session_data['token']
+        return None
+
+    def _save_session(self, token):
+        session = {'lastSessionDate' : datetime.datetime.now(), 'token' : token}
+        filehandler = open(self.session_file, 'wb')
+        pickle.dump(session, filehandler)
+        filehandler.close()
 
     def _instrument_row(self, instruments, stock, exchange='NSE'):
         for instrument_data in instruments:
