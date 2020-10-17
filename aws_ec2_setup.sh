7cd62735b2cd3e558b847d5f543451d98dcebcb5
Ubuntu <ubuntu@ip-172-31-64-30.ec2.internal>
2020-10-17 13:36:03 +0000

    Adding the setup for AWS Ec2

---
+++ aws_ec2_setup.sh
@@ -0,0 +1,8 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+python3 -m pip install virtualenv
+python3 -m virtualenv -ppython3 stock_trading_env
+sudo apt-get install build-essential libffi-dev python-dev-is-python3 python3-dev
+source stock_trading_env/bin/activate
+pip3 install matplotlib kiteconnect findiff numpy python-telegram-bot
+
---
+++ sample_back_test_script.sh
@@ -0,0 +1,2 @@
+python3 ./AutomatedTradingSystem.py -l RELIANCE -s "2020-10-01" -e "2020-10-15" audit
+
